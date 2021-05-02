8e5d49dc4451105cbd26842d42db40b14930b653
Ramón David Sifuentes <ramon@gobusinessinc.com>
2021-05-02 12:22:11 -0500

    Simple Gate as a class

--- SimpleGate.py
+++ SimpleGate.py
@@ -4,27 +4,26 @@ from time import sleep
 
 class SimpleGate():
 
-    timeInterval = 0.1 # Duty cycle time when monitoring the opening state in seconds
-    timeOut = 2*60 # max amount of time of operation in seconds (use it for security)
-
-    def __init__(self, endSensorPin=11, openSignalPin=17):
-        self.endSensor = Button(endSensor) # GPIO that will be activated once the gat reach the end of the reel
+    def __init__(self, endSensorPin=11: int, openSignalPin=17: int, timeInterval = 0.1: float, timeOut = 5*60 :float):
+        self.timeInterval = timeInterval # Duty cycle time when monitoring the opening state in seconds
+        self.timeOut = timeOut # max amount of time of operation in seconds (use it for security)
+        self.endSensor = Button(endSensorPin) # GPIO that will be activated once the gat reach the end of the reel
         self.openSignal = LED(openSignalPin) # GPIO that will be activated to turn on the engine towards open direction
 
     # Open action will set ON the pin that opens the gate till the end of line sensor is activated or timeout
-    def open(timeOperation = None):
+    def openGate(self, timeOperation = None):
         maxTime = 0
         if(timeOperation != None):
             maxTime = timeOperation
         else:
-            maxTime = timeOut
+            maxTime = self.timeOut
         self.openSignal.on()
         print("Open signal received")
         count = 0
         while ( self.endSensor.is_pressed != True ):
-            sleep(timeInterval)
+            sleep(self.timeInterval)
             count += 1
-            if(count * timeInterval >maxTime ):
+            if(count * self.timeInterval >maxTime ):
                 self.openSignal.off()
                 print("Timeout!")
                 return False
--- open.py
+++ open.py
@@ -1,34 +1,5 @@
-from gpiozero import LED
-from gpiozero import Button
-from time import sleep
-
-# Open actions will set ON the pin that opens the gate till the end of line sensor is activated
-
-openButton = Button(4) # GPIO assigned to receive the open signal
-endSensor = Button(11) # GPIO that will be activated once the gat reach the end of the reel
-openPin = LED(17) # GPIO that will be activated to turn on the engine towards open direction
-timeInterval = 0.1 # Duty cycle time when monitoring the opening state in seconds
-timeOut = 2*60 # max amount of time of operation in seconds (use it for security)
-
-def open(timeOperation = None):
-    maxTime = 0
-    if(timeOperation != None):
-        maxTime = timeOperation
-    else:
-        maxTime = timeOut
-    openPin.on()
-    print("Open signal received")
-    count = 0
-    while ( endSensor.is_pressed != True ):
-        sleep(timeInterval)
-        count += 1
-        if(count * timeInterval >maxTime ):
-            openPin.off()
-            print("Timeout!")
-            return False
-    openPin.off()
-    print("Gate fully opened")
-    return True
+from SimpleGate import SimpleGate
 
 if __name__ == "__main__":
-    open()
+    gate = SimpleGate(11, 17, 0.1, 5)
+    gate.openGate()
